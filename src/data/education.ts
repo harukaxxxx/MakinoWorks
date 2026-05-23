0e4ddd4aae27201e81f1803e707f5295267afe72
Makino Fuyuki <makino@futsuki.local>
2026-05-24 00:57:25 +0800

    🐞 fix: update career and education data

--- src/data/education.ts
+++ src/data/education.ts
@@ -2,14 +2,14 @@ import type { Experience } from "@/lib/types";
 
 export const education: Experience[] = [
   {
-    id: "ntut",
+    id: "cyut",
     period: "2014.09 — 2017.08",
     title: "建築及都市設計碩士班",
     organization: "朝陽科技大學",
     description: "",
   },
   {
-    id: "ntut-bachelor",
+    id: "ntut",
     period: "2009.09 — 2014.07",
     title: "創意設計學士班",
     organization: "國立臺北科技大學",
